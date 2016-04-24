de8ba913b771fe588d03178a703c6ded58999b43
Alvaro Garcia <derecerca@gmail.com>
2016-04-24 12:22:14 +0200

    refactor: split loop by responsibility
    creating employees and sending them greetings are in different loops

--- src/main/java/com/dodevjutsu/kata/birthdaygreetings/BirthdayService.java
+++ src/main/java/com/dodevjutsu/kata/birthdaygreetings/BirthdayService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -21,10 +23,14 @@ public class BirthdayService {
         BufferedReader in = new BufferedReader(new FileReader(fileName));
         String str = "";
         str = in.readLine(); // skip header
+        List<Employee> employees = new ArrayList();
         while ((str = in.readLine()) != null) {
             String[] employeeData = str.split(", ");
             Employee employee = new Employee(employeeData[1], employeeData[0],
                     employeeData[2], employeeData[3]);
+            employees.add(employee);
+        }
+        for (Employee employee : employees) {
             if (employee.isBirthday(ourDate)) {
                 String recipient = employee.getEmail();
                 String body = "Happy Birthday, dear %NAME%!".replace("%NAME%",
