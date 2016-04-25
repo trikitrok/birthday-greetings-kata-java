18db2ee4283edc5af8554e0e1722447ed165e88e
Alvaro Garcia <derecerca@gmail.com>
2016-04-25 08:37:42 +0200

    green: throw an exception when the file is not found

---
+++ src/test/java/com/dodevjutsu/kata/birthdaygreetings/FileEmployeeRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.dodevjutsu.kata.birthdaygreetings;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.StringContains;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.*;
+
+public class FileEmployeeRepositoryTest {
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
+    @Test
+    public void fails_when_the_file_does_not_exist() throws Exception {
+
+        FileEmployeeRepository service = new FileEmployeeRepository("non-existing.file");
+        expected.expect(CannotReadEmployeesException.class);
+        expected.expectMessage(containsString("cannot read file"));
+        expected.expectMessage(containsString("non-existing.file"));
+
+        service.whoseBirthdayIsOn(new OurDate("2016/01/01"));
+    }
+
+}
