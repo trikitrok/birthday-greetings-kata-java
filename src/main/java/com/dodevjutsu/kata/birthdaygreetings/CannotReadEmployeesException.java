7860d088c2710ce3b30396042877cbef478eb8c4
Alvaro Garcia <derecerca@gmail.com>
2016-04-25 22:06:28 +0200

    refactor: remove unused imports

--- src/main/java/com/dodevjutsu/kata/birthdaygreetings/CannotReadEmployeesException.java
+++ src/main/java/com/dodevjutsu/kata/birthdaygreetings/CannotReadEmployeesException.java
@@ -1,7 +1,5 @@
 package com.dodevjutsu.kata.birthdaygreetings;
 
-import java.io.FileNotFoundException;
-
 public class CannotReadEmployeesException extends RuntimeException {
     public CannotReadEmployeesException(String cause, Exception exception) {
         super(cause, exception);
