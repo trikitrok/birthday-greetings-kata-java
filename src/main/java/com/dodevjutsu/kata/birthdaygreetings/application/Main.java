46fae70600a220412bd726e2b2626037a87efce9
trikitrok <trikitrok@gmail.com>
2017-02-02 16:54:46 +0100

    refactor: adding some constants

--- src/main/java/com/dodevjutsu/kata/birthdaygreetings/application/Main.java
+++ src/main/java/com/dodevjutsu/kata/birthdaygreetings/application/Main.java
@@ -1,5 +1,6 @@
 package com.dodevjutsu.kata.birthdaygreetings.application;
 
+import com.dodevjutsu.kata.birthdaygreetings.core.OurDate;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailConfiguration;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailGreetingsSender;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailSender;
@@ -7,15 +8,24 @@ import com.dodevjutsu.kata.birthdaygreetings.infrastructure.repositories.DateRep
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.repositories.FileEmployeeRepository;
 
 public class Main {
+
+    private static final String EMPLOYEES_FILE_PATH = "employee_data.txt";
+    private static final String SENDER_EMAIL_ADDRESS = "sender@here.com";
+    private static final String HOST = "localhost";
+    private static final int SMTP_PORT = 25;
+    private static final String TODAY_AS_STRING = "2008/10/08";
+
     public static void main(String[] args) {
         BirthdayService service = new BirthdayService(
-            new FileEmployeeRepository("employee_data.txt"),
-            new EmailGreetingsSender(new EmailConfiguration("sender@here.com", "localhost", 25), new EmailSender())
+            new FileEmployeeRepository(EMPLOYEES_FILE_PATH),
+            new EmailGreetingsSender(
+                new EmailConfiguration(SENDER_EMAIL_ADDRESS, HOST, SMTP_PORT),
+                new EmailSender()
+            )
         );
         try {
-            service.sendGreetings(
-                new DateRepresentation("2008/10/08").toDate()
-            );
+            OurDate today = new DateRepresentation(TODAY_AS_STRING).toDate();
+            service.sendGreetings(today);
         } catch (Exception e) {
             e.printStackTrace();
         }
