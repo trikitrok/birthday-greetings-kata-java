50a649d73224f08df19ca96c1e610105c00b8757
trikitrok <trikitrok@gmail.com>
2017-02-02 16:49:38 +0100

    using DateRepresentation in main

--- src/main/java/com/dodevjutsu/kata/birthdaygreetings/application/Main.java
+++ src/main/java/com/dodevjutsu/kata/birthdaygreetings/application/Main.java
@@ -1,12 +1,10 @@
 package com.dodevjutsu.kata.birthdaygreetings.application;
 
-import com.dodevjutsu.kata.birthdaygreetings.core.OurDate;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailConfiguration;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailGreetingsSender;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.greetings_senders.by_email.EmailSender;
+import com.dodevjutsu.kata.birthdaygreetings.infrastructure.repositories.DateRepresentation;
 import com.dodevjutsu.kata.birthdaygreetings.infrastructure.repositories.FileEmployeeRepository;
-
-import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,9 +14,7 @@ public class Main {
         );
         try {
             service.sendGreetings(
-                new OurDate(
-                    new SimpleDateFormat("yyyy/MM/dd").parse("2008/10/08")
-                )
+                new DateRepresentation("2008/10/08").toDate()
             );
         } catch (Exception e) {
             e.printStackTrace();
