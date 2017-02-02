6d274385c16793a27440514a7dd6ef999a3ee8a0
trikitrok <trikitrok@gmail.com>
2017-02-02 16:30:28 +0100

    refactor: renaming test

--- src/test/java/com/dodevjutsu/kata/birthdaygreetings/test/OurDateTest.java
+++ src/test/java/com/dodevjutsu/kata/birthdaygreetings/test/OurDateTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class OurDateTest {
     @Test
-    public void isSameDate() throws Exception {
+    public void identifies_if_two_dates_were_in_the_same_day() throws Exception {
         OurDate ourDate = new DateRepresentation("1789/01/24").toDate();
         OurDate sameDay = new DateRepresentation("2001/01/24").toDate();
         OurDate notSameDay = new DateRepresentation("1789/01/25").toDate();
